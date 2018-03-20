017287493caa60494b07f6a1acca3894e6721f32
Sidath Asiri <sidathasiri@gmail.com>
2018-03-20 19:06:58 +0530

    added catchment_area and overburden

--- create_dataset.py
+++ create_dataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from numpy import column_stack
 from sklearn.preprocessing import normalize
 
 landslides = pd.read_csv("./datasets/landslides.csv")
@@ -7,6 +8,8 @@ landform = pd.read_csv("./datasets/landform.csv")
 landuse = pd.read_csv("./datasets/landuse.csv")
 vegetation = pd.read_csv("./datasets/vegetation.csv")
 drainage_density = pd.read_csv("./datasets/drainage_density.csv")
+overburden = pd.read_csv("./datasets/overburden.csv")
+catchment_area = pd.read_csv("./datasets/catchment_area.csv")
 
 landslides = landslides.replace(255, 0)
 landslides = landslides.replace([1, 2, 3, 4, 5, 6, 7, 8], 1)
@@ -17,6 +20,8 @@ print(landform.shape)
 print(landuse.shape)
 print(vegetation.shape)
 print(drainage_density.shape)
+print(overburden.shape)
+print(catchment_area.shape)
 
 def extract_data_points(layer):
     set1 = layer.ix[:499, :800]
@@ -42,6 +47,8 @@ landslides_data = extract_data_points(landslides)
 landuse_data = extract_data_points(landuse)
 vegetation_data = extract_data_points(vegetation)
 drainage_density_data = extract_data_points(drainage_density)
+overburden_data = extract_data_points(overburden)
+catchment_area_data = extract_data_points(catchment_area)
 
 print("After extract")
 print(slope_data.shape)
@@ -50,15 +57,20 @@ print(landslides_data.shape)
 print(landuse_data.shape)
 print(vegetation_data.shape)
 print(drainage_density_data.shape)
+print(overburden_data.shape)
+print(catchment_area_data.shape)
 
 # print("slope\n", slope_data.head())
 # print("landform\n", landform_data.head())
 
-inputData = pd.concat([slope_data, landform_data, landuse_data, vegetation_data, drainage_density_data], axis=1)
+inputData = pd.concat([slope_data, landform_data, landuse_data, vegetation_data, drainage_density_data, overburden_data, catchment_area_data], axis=1)
 #inputData = normalize(inputData)
 #inputData = pd.DataFrame(inputData, columns=["slope", "landform", "landuse", "vegetation"])
 # print("input\n", inputData.head())
 
+inputData.columns = ["slope", "landform", "landuse", "vegetation", "drainage", "overburden", "catchment_area"]
 inputData.to_csv("input_data.csv", index=False)
 landslides_data.to_csv("outputs.csv", index=False)
+
+
 print("done")
