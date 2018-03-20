79953df06a839ede5901901734c3c0b25d24d0ba
Sidath Asiri <sidathasiri@gmail.com>
2018-03-20 19:58:16 +0530

    added elevation

--- create_dataset.py
+++ create_dataset.py
@@ -10,6 +10,7 @@ vegetation = pd.read_csv("./datasets/vegetation.csv")
 drainage_density = pd.read_csv("./datasets/drainage_density.csv")
 overburden = pd.read_csv("./datasets/overburden.csv")
 catchment_area = pd.read_csv("./datasets/catchment_area.csv")
+elevation = pd.read_csv("./datasets/elevation.csv")
 
 landslides = landslides.replace(255, 0)
 landslides = landslides.replace([1, 2, 3, 4, 5, 6, 7, 8], 1)
@@ -22,6 +23,7 @@ print(vegetation.shape)
 print(drainage_density.shape)
 print(overburden.shape)
 print(catchment_area.shape)
+print(elevation.shape)
 
 def extract_data_points(layer):
     set1 = layer.ix[:499, :800]
@@ -49,6 +51,7 @@ vegetation_data = extract_data_points(vegetation)
 drainage_density_data = extract_data_points(drainage_density)
 overburden_data = extract_data_points(overburden)
 catchment_area_data = extract_data_points(catchment_area)
+elevation_data = extract_data_points(elevation)
 
 print("After extract")
 print(slope_data.shape)
@@ -59,16 +62,17 @@ print(vegetation_data.shape)
 print(drainage_density_data.shape)
 print(overburden_data.shape)
 print(catchment_area_data.shape)
+print(elevation_data.shape)
 
 # print("slope\n", slope_data.head())
 # print("landform\n", landform_data.head())
 
-inputData = pd.concat([slope_data, landform_data, landuse_data, vegetation_data, drainage_density_data, overburden_data, catchment_area_data], axis=1)
+inputData = pd.concat([slope_data, landform_data, landuse_data, vegetation_data, drainage_density_data, overburden_data, catchment_area_data, elevation_data], axis=1)
 #inputData = normalize(inputData)
 #inputData = pd.DataFrame(inputData, columns=["slope", "landform", "landuse", "vegetation"])
 # print("input\n", inputData.head())
 
-inputData.columns = ["slope", "landform", "landuse", "vegetation", "drainage", "overburden", "catchment_area"]
+inputData.columns = ["slope", "landform", "landuse", "vegetation", "drainage", "overburden", "catchment_area", "elevation"]
 inputData.to_csv("input_data.csv", index=False)
 landslides_data.to_csv("outputs.csv", index=False)
 
